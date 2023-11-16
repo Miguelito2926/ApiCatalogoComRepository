4abecd837a8d4e439dde3d83ed63931d4f8d60b0
Miguelito2926 <juniorprol@hotmail.com>
2023-11-16 16:22:50 -0300

    Criando Interface Generica IRepository

--- ApiCatalogoComRepository/Repository/IRepository.cs
+++ ApiCatalogoComRepository/Repository/IRepository.cs
@@ -1,7 +1,13 @@
-﻿namespace ApiCatalogoComRepository.Repository
-{
-    public interface IRepository
-    {
+﻿using System.Linq.Expressions;
 
+namespace ApiCatalogoComRepository.Repository
+{
+    public interface IRepository<T>
+    {
+        IQueryable<T> GetAll();
+        T GetById(Expression<Func<T, bool>> predicate);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
     }
 }
